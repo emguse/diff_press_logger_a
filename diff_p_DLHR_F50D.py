99b75e22b04d9c6adee3418686a15818cf3823a6
emguse <30695541+emguse@users.noreply.github.com>
2021-11-26 20:35:44 +0900

    first commit

---
+++ diff_p_DLHR_F50D.py
@@ -0,0 +1,105 @@
+from smbus2 import SMBus
+import time
+
+# All sensoers (Amphenol) DLHR-F50D-E1BD-C-NAV8
+# for Raspberry Pi
+# 2021/11/04 ver.1.0
+
+ADDRESS = 0x29
+
+OS_DIG = 0.5 * 2 ** 24 # For Differential Operating Range sensors
+FSS_IN_H2O = 2 * 0.5 # For Differential Operating Range sensors : 2 x Full Scale Pressure.
+PA_CONVERSION = 249.089 # Conversion factor from "inH2O" to "Pa"
+FSS_PA = FSS_IN_H2O * PA_CONVERSION # FSS/2 for "Pa"
+ZERO_OFFSET = 2 # Zero point correction
+
+START_SINGLE = 0xAA
+START_AVERAGE2 = 0xAC
+START_AVERAGE4 = 0xAD
+START_AVERAGE8 = 0xAE
+START_AVERAGE16 = 0xAF
+
+class DLHR_F50D():
+    def __init__(self) -> None:
+        self.address = ADDRESS
+        self.status = 0x00
+        self.power = False
+        self.busy = False
+        self.mode = 0x00
+        self.memory_error = False
+        self.alu_error = False
+        self.rawp = 0
+        self.rawt = 0
+        self.pressure = 0
+        self.temprature = 0
+        self.bus = SMBus(1)
+        pass
+
+    def send_start(self):
+        time.sleep(0.0005)
+        self.bus.write_byte(self.address, START_AVERAGE2)
+    
+    def status_read(self):
+        self.status = self.bus.read_byte(self.address)
+        if self.status & 0x40 == 0x40:
+            self.power = True
+        else:
+            self.power = False
+        if self.status & 0x20 == 0x20:
+            self.busy = True
+        else:
+            self.busy = False
+        self.mode = (self.status and 0x18) >> 3
+        if self.status & 0x04 == 0x04:
+            self.memory_error = True
+        else:
+            self.memory_error = False
+        if self.status & 0x01 == 0x01:
+            self.alu_error = True
+        else:
+            self.alu_error = False
+
+    def read_busy(self) -> bool:
+        self.status = self.bus.read_byte(self.address)
+        if self.status & 0x20 == 0x20:
+            self.busy = True
+        else:
+            self.busy = False
+        return self.busy
+    
+    def poll_busy(self):
+        while self.read_busy():
+            pass
+    
+    def correction_p(self):
+        self.pressure = 1.25 * ((self.rawp - OS_DIG)/ 2**24) * FSS_PA
+
+    def correction_t(self):
+        self.temprature = (self.rawt * 125)/ 2**24 - 40
+
+    def read(self):
+        data = self.bus.read_i2c_block_data(self.address, 0x00, 7)
+        self.rawp = (data[1] << 16) | (data[2] << 8) | data[3]
+        self.rawt = (data[4] << 16) | (data[5] << 8) | data[6]
+
+    def read_p(self):
+        self.send_start()
+        self.poll_busy()
+        self.read()
+        self.correction_p()
+    
+    def read_t(self):
+        self.send_start()
+        self.poll_busy()
+        self.read()
+        self.correction_t()
+
+def main(): # Sample usage
+    dlhr_f50d = DLHR_F50D()
+    while True:
+        dlhr_f50d.read_p()
+        print("pressure :" + str(round(dlhr_f50d.pressure - ZERO_OFFSET ,4)))
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
---
+++ diff_press_logger_a.py
@@ -0,0 +1,115 @@
+import diff_p_DLHR_F50D as DLHR_F50D
+from polling_timer import PollingTimer
+from move_ave import MovingAverage
+from collections import deque
+from wave_save import WavSave
+from buzz_pipi_r import PiPi
+from print_with_DP_EH600 import PrintWithDpEh600
+import datetime
+import multiprocessing as mp
+
+SAMPLE_FREQ = 32 # Hz
+SAMPLE_INTERVAL = 1/SAMPLE_FREQ # 
+IVENT_LENGTH = 30 # sec
+QUE_SIZE = int(IVENT_LENGTH/2 * SAMPLE_FREQ)
+MOVE_AVE_LENGTH = 4
+REFARENCE_PAST_SAMPLE = 4
+THRESHOLD = 0.4
+MAX_VALUE = 125
+SAVE_DIR = './log/'
+
+ZERO_OFFSET = 1.9 # Zero point correction
+USE_PRINTER = True
+USE_CSV_EXPORT = True
+USE_WAV_EXPORT = False
+
+def thermal_printing(data) -> None:
+    if USE_PRINTER == True:
+        for s in data:
+            p = PrintWithDpEh600()
+            p.printing(s)
+        p.line_feed(1)
+
+def sound_buzzer():
+    pipi = PiPi()
+    pipi.pipi()
+
+def export_csv(d_a):
+    now = datetime.datetime.now()
+    now_iso = now.strftime('%Y-%m-%d') + 'T' + now.strftime('%H_%M_%S_%f')
+    filename = str(SAVE_DIR + now_iso + '.csv')
+    try:
+        with open(filename, 'w', newline='') as f:
+            #writer = csv.writer(f)
+            print("Start exporting data")
+            for i in d_a:
+                #writer.writerow(i)
+                f.write(str(i)+'\n')
+            print("Export Complete")
+    except:
+        print("File export error")
+
+def main():
+    dlhr_f50d = DLHR_F50D.DLHR_F50D()
+    read_intarval = PollingTimer(SAMPLE_INTERVAL)
+    record_intarval = PollingTimer(IVENT_LENGTH/2)
+    record_intarval.up_state = True
+    ma = MovingAverage(MOVE_AVE_LENGTH)
+    dq_p = deque(maxlen=QUE_SIZE)
+    dq_ref = deque(maxlen=REFARENCE_PAST_SAMPLE)
+    dq_after = deque(maxlen=QUE_SIZE)
+
+    wavesave = WavSave()
+    wavesave.set_wav_param(1,2,SAMPLE_FREQ)
+    wavesave.set_norm(MAX_VALUE)
+
+    for _ in range(MOVE_AVE_LENGTH):
+        while True:
+            read_intarval.timer_update()
+            if read_intarval.up_state == True:
+                dlhr_f50d.read_p()
+                ma_p = ma.simple_moving_average(dlhr_f50d.pressure - ZERO_OFFSET)
+                dq_p.append(ma_p)
+                dq_ref.append(ma_p)
+                break
+
+    while True:
+        read_intarval.timer_update()
+        if read_intarval.up_state == True:
+            dlhr_f50d.read_p()
+            ma_p = ma.simple_moving_average(dlhr_f50d.pressure - ZERO_OFFSET)
+            dq_p.append(ma_p)
+            dq_ref.append(ma_p)
+            if abs(dq_ref[0])+THRESHOLD <= abs(ma_p):
+                record_intarval.timer_update_only()
+                if record_intarval.up_state == True:
+                    record_intarval.up_state = False
+
+                    buzzer_process = mp.Process(target=sound_buzzer)
+                    buzzer_process.start()
+                    
+                    data = []
+                    now = datetime.datetime.now()
+                    data.append(now.strftime('%Y-%m-%d') + 'T' + now.strftime('%H:%M:%S.%f'))
+                    data.append('DP:' + str(round(ma_p, 4)) + '  ΔP:' + str(round((dq_ref[0] - ma_p),4)))
+                    print_process = mp.Process(target=thermal_printing, args=(data,))
+                    print_process.start()
+
+                    for i in range(QUE_SIZE):
+                        while True:
+                            read_intarval.timer_update()
+                            if read_intarval.up_state == True:
+                                dlhr_f50d.read_p()
+                                ma_p = ma.simple_moving_average(dlhr_f50d.pressure - ZERO_OFFSET)
+                                dq_after.append(ma_p)
+                                dq_ref.append(ma_p)
+                                break
+                    ar = list(dq_p)
+                    ar.extend(dq_after)
+                    if USE_WAV_EXPORT == True:
+                        wavesave.save_w_date(dq_p)
+                    if USE_CSV_EXPORT == True:
+                        export_csv(ar)
+
+if __name__ == '__main__':
+    main()
